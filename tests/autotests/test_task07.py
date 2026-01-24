52a752bd5cccd29dc3edca14d751637a804c9710
Nikita Shchutskii <nshchutskiy@gmail.com>
2026-01-24 20:13:32 +0300

    format

--- tests/autotests/test_task07.py
+++ tests/autotests/test_task07.py
@@ -11,7 +11,7 @@ from cfpq_concrete_cases import CASES_CFPQ, CaseCFPQ
 
 # Fix import statements in try block to run tests
 try:
-    from project.cfpq import hellings_based_cfpq,  matrix_based_cfpq
+    from project.cfpq import hellings_based_cfpq, matrix_based_cfpq
 except ImportError:
     pytestmark = pytest.mark.skip("Task 7 is not ready to test!")
 
