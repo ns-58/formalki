817f64072e13f64a3129189dd66341c8ffb2b8f8
Nikita Shchutskii <nshchutskiy@gmail.com>
2026-04-14 17:22:25 +0300

    fix: repair test: make singleton

--- tests/test_none.py
+++ tests/test_none.py
@@ -7,7 +7,9 @@ def test_hel_none_none():
     assert hel.hellings_based_cfpq(
         CFG.from_text("S -> S a | $"),
         graphs.point_graph,
-    ) == (
-        1,
-        1,
-    )
+    ) == {
+        (
+            1,
+            1,
+        )
+    }
