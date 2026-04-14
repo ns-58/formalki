eb836d778b4281780e4524506da2dd6abcdb744b
Nikita Shchutskii <nshchutskiy@gmail.com>
2026-04-14 17:08:06 +0300

    fix: repair test: change grammar

--- tests/test_none.py
+++ tests/test_none.py
@@ -1,11 +1,13 @@
 import project.hellings as hel
 import graphs
 from pyformlang.cfg import CFG
-import pytest
 
 
 def test_hel_none_none():
-    assert hel.hellings_based_cfpq( CFG.from_text("S -> a"), graphs.point_graph,) == (
+    assert hel.hellings_based_cfpq(
+        CFG.from_text("S -> S a | $"),
+        graphs.point_graph,
+    ) == (
         1,
         1,
     )
